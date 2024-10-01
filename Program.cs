31bfde44e798c08f60fac49a78a526cd25a644e6
kodpelle <pellewiklund@outlook.com>
2024-10-01 10:30:19 +0200

    skapat en metod för att enkelt kunna söka i listan, få bort kod från main och enklare att återanvända

--- Program.cs
+++ Program.cs
@@ -8,8 +8,10 @@ namespace NameSorter
 {
     public class ListKlass
     {
+        //Jag behåller datastrukturen lista eftersom vi bara hanterar strings med namn, det är enklast och mest effektivt när det är en liten simpel lista.
         List<string> names = new List<string> { "Anna", "John", "Alice", "Börje" };
 
+        //ett förbättrat sätt att skriva ut innehållet i listan, går att återanvända
         public void PrintNames()
         {
             foreach (var name in names)
@@ -17,25 +19,39 @@ namespace NameSorter
                 Console.WriteLine(name);
             }
         }
+        //om man vill sortera listan så kan man skriva en metod, mindre kod i main.
         public void SortedNames()
         {
             Console.WriteLine("Alfabetiskt sorterad lista:");
             names.Sort();
             PrintNames();
         }
+        //Man kanske vill lägga till någon i listan då kan man göra det i konsollen
         public void AddToList()
         {
             Console.WriteLine("skriv vilket namn du vill lägga till");
             string addname = Console.ReadLine().ToLower();
             names.Add(addname);
         }
+        //Metod för att söka i listan
+        public void SearchName(string FindName)
+        {
+            if (names.Contains(FindName))
+            {
+                Console.WriteLine($"{FindName} Finns i listan.");
+            }
+            else
+            {
+                Console.WriteLine($"{FindName} Finns inte i listan.");
+            }
+            
+        }
     }
      class Program
     {
         static void Main(string[] args)
         {
-            
-
+            /*
 
             Console.WriteLine("\nEnter name to search:");
             string searchName = Console.ReadLine();
@@ -46,7 +62,7 @@ namespace NameSorter
             else
             {
                 Console.WriteLine($"{searchName} is not in the list. ");
-            }
+            }*/
             Console.ReadKey();
         }
         
