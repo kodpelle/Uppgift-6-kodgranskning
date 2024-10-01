7251161b3918ae954b7427dfda73652f5383d5f1
kodpelle <pellewiklund@outlook.com>
2024-10-01 09:30:30 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameSorter
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string> { "Anna", "John", "Alice", "Börje" };
+            Console.WriteLine("Original list");
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            names.Sort();
+            Console.WriteLine("\nSorted list:");
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine("\nEnter name to search:");
+            string searchName = Console.ReadLine();
+            if (names.Contains(searchName))
+            {
+                Console.WriteLine($"{searchName} is in the list.");
+            }
+            else
+            {
+                Console.WriteLine($"{searchName} is not in the list. ");
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
